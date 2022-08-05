f6f24abab474c6bf1b0acf2814ea6d5b78e5b0b4
Brad Czerniak <ao5357@gmail.com>
2022-08-05 12:28:09 -0400

    Resolves #7 by making a search toggle for desktop.

--- docroot/modules/custom/frost_custom/src/Plugin/Block/SearchIconBlock.php
+++ docroot/modules/custom/frost_custom/src/Plugin/Block/SearchIconBlock.php
@@ -77,8 +77,27 @@ class SearchIconBlock extends BlockBase implements ContainerFactoryPluginInterfa
     $form['#action'] = '/search';
 
     // Render search form.
+    $icon = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M15.7 13.3l-3.81-3.83A5.93 5.93 0 0013 6c0-3.31-2.69-6-6-6S1 2.69 1 6s2.69 6 6 6c1.3 0 2.48-.41 3.47-1.11l3.83 3.81c.19.2.45.3.7.3.25 0 .52-.09.7-.3a.996.996 0 000-1.41v.01zM7 10.7c-2.59 0-4.7-2.11-4.7-4.7 0-2.59 2.11-4.7 4.7-4.7 2.59 0 4.7 2.11 4.7 4.7 0 2.59-2.11 4.7-4.7 4.7z"/></svg>';
     $build['search_icon_block'] = [
-      '#markup' => '<span class="search-icon icon magnifying-glass"></span>',
+      '#attributes' => [
+        'class' => [
+          'block--search-icon',
+        ],
+      ],
+      'icon' => [
+        '#type' => 'inline_template',
+        '#allowed_tags' => [
+          'button',
+          'path',
+          'span',
+          'svg',
+        ],
+        '#template' => '<button aria-expanded="false" '
+        . 'class="js--toggle-below button--search-widget--toggle" type="button">'
+        . $icon
+        . '<span class="js--toggle-below--label a11y--visually-hidden">Open below</span>'
+        . '</button>',
+      ],
       'form_wrapper' => [
         '#type' => 'container',
         '#attributes' => [
